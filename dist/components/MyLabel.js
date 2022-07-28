8120896899cceab7d633443fb5639b85d6bb8ea6
Nahuel Clotet <nahuelclotet@gmail.com>
2022-07-28 17:06:20 -0300

    fix: package.json

--- dist/components/MyLabel.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './mylabel.css';
-export const MyLabel = ({ label = 'Basic', size = 'normal', allCaps = false, color = 'primary', fontColor }) => (_jsx("span", { className: `label ${size} ${color && `text-${color}`}`, style: { textTransform: allCaps ? 'uppercase' : 'none',
-        color: fontColor ? fontColor : ''
-    }, children: label }));
