bf4edd60af488e20a0cafa2172979d70627b015a
Nahuel Clotet <nahuee2011@gmail.com>
2022-07-28 17:06:20 -0300

    fix: package.json

--- dist/main.d.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from "./stories/Header";
-export { Page } from "./stories/Page";
-export { Button } from "./stories/Button";
-export { MyLabel } from './components/MyLabel';
--- dist/stories/Page.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './page.css';
-export declare type User = {
-    name: string;
-};
-export declare const Page: React.VFC;
